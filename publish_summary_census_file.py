bc286631534e757562a41e17438a9cccfed6672f
Janos G. Hajagos <risk.limits@gmail.com>
2015-02-01 19:58:50 -0500

    Added abridged

--- publish_summary_census_file.py
+++ publish_summary_census_file.py
@@ -21,9 +21,19 @@ def pad_number(number, length):
 
 
 def main(table_to_publish, directory, state="NY", reference_year="2012", period_covered="5", iteration="000",
-         file_types=["e", "m"]):
+         file_types=["e", "m"], abridged=False):
 
-    # Sequence number mapping
+    """
+        file_types :
+            e=estimate, m=margin of error
+        reference year
+        period_covered="5" is the 5 year file
+        iteration="000"
+
+        abridged:
+            Supports two formats for the table. A full version which has repetitive details and an abridged version which has
+            only unique elements.
+    """
 
     print("Loading table to sequence file mappings")
     with open("./support_files/table_number_to_sequence_number.json") as fj:
@@ -56,7 +66,12 @@ def main(table_to_publish, directory, state="NY", reference_year="2012", period_
             variable_mapping_information_dict[table_to_publish] = {}
 
         for file_type in file_types:
-            file_to_write = os.path.join(directory, file_type + reference_year + period_covered + state.lower() + table_to_publish + ".csv")
+            if abridged:
+                abridged = "a_"
+            else:
+                abridged = ""
+
+            file_to_write = os.path.join(directory, abridged + file_type + reference_year + period_covered + state.lower() + table_to_publish + ".csv")
             file_to_read = os.path.join(directory, file_type + suffix_file)
 
             FIELD_LAYOUT = ["FILEDID", "FILETYPE", "STUSAB", "CHARITER", "SEQUENCE", "LOGRECNO"]
@@ -79,37 +94,47 @@ def main(table_to_publish, directory, state="NY", reference_year="2012", period_
                     print("Writing CSV file '%s'" % file_to_write)
                     with open(file_to_write, "wb") as fw:
                         cwr = csv.writer(fw)
-                        cwr.writerow(["goeid", "geo_name", "table_id", "table_name", "subject area",
-                                      "relative_position", "context_path", "file_type", "field_name", "value"])
+
+                        if abridged:
+                            cwr.writerow(["geoid", "table_id", "relative_position", "value"])
+                        else:
+                            cwr.writerow(["goeid", "geo_name", "table_id", "table_name", "subject area",
+                                          "relative_position", "context_path", "file_type", "field_name", "value"])
 
                         for row in cr:
                             logical_record = row[field_dict["LOGRECNO"]]
                             if logical_record in geographic_data:
                                 geographic = geographic_data[logical_record]
                                 geoid = geographic["GEOID"]
-                                geo_name = geographic["NAME"]
-                                row_to_write_template = [geoid, geo_name]
+                                row_to_write_template = [geoid]
+                                if not abridged:
+                                    geo_name = geographic["NAME"]
+                                    row_to_write_template += [geo_name]
+
                                 row_to_write_template += [table_to_publish]
 
-                                if "subject area" in table_data:
-                                    subject = table_data["subject area"]
-                                else:
-                                    subject = ""
+                                if not abridged:
+                                    if "subject area" in table_data:
+                                        subject = table_data["subject area"]
+                                    else:
+                                        subject = ""
 
-                                if "table name" in table_data:
-                                    table_name = table_data["table name"]
-                                else:
-                                    table_name = ""
+                                    if "table name" in table_data:
+                                        table_name = table_data["table name"]
+                                    else:
+                                        table_name = ""
 
-                                row_to_write_template += [table_name, subject]
+                                    row_to_write_template += [table_name, subject]
 
-                                for field in table_data["fields"]:
+                                for field in table_data["fields"]: #Iterate through all fields associated with a table
                                     row_to_write = list(row_to_write_template)
                                     relative_position = field["relative position"]
-                                    context_path = "|".join(field["context path"])
-                                    field_name = field["field name"]
+                                    row_to_write += [relative_position]
+                                    if not abridged:
+                                        context_path = "|".join(field["context path"])
+                                        field_name = field["field name"]
+                                        row_to_write += [context_path, file_type, field_name]
 
-                                    row_to_write += [relative_position, context_path, file_type, field_name]
                                     absolute_position = field["table position"] - 1
                                     try:
                                         value = row[absolute_position]
@@ -120,6 +145,7 @@ def main(table_to_publish, directory, state="NY", reference_year="2012", period_
 
                                     row_to_write += [value]
                                     cwr.writerow(row_to_write)
+
             else:
                 print("File '%s' does not exist" % file_to_read)
     else:
@@ -137,4 +163,4 @@ if __name__ == "__main__":
 
     acs_fields = config.acs_fields_to_export
     for acs_field in acs_fields:
-        main(acs_field, config.geography_directory)
+         main(acs_field, config.geography_directory)
