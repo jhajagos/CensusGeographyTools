191b403fb5edddb10748e34a8741621063566af9
janos <risk.limits@gmail.com>
2020-09-12 11:51:38 -0400

    Built out florida example

--- acs_examples/acs_variables_select_us_states_and_counties.py
+++ acs_examples/acs_variables_select_us_states_and_counties.py
@@ -8,13 +8,14 @@ import os
 def main(config, output_directory="./"):
 
     state_query_dict = {
-        "tx": """select distinct geoid, geo_name from acs_summary_2018tx."e20185txB00001" where geoid like '05000US48%%' order by geo_name"""
+        "tx": """select distinct geoid, geo_name from acs_summary_2018tx."e20185txB00001" where geoid like '05000US48%%' order by geo_name""",
+        "fl": """select distinct geoid, geo_name from acs_summary_2018fl."e20185flB00001" where geoid like '05000US12%%' order by geo_name"""
     }
 
     engine = sa.create_engine(config["connection_uri"])
     schema_dict = config["schemas"]
     with engine.connect() as connection:
-        state_list = ["tx"]
+        state_list = ["tx", "fl"]
 
         for state in state_list:
             state_query = state_query_dict[state]
